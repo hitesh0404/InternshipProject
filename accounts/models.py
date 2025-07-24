b4604a2e5c2b7ec1c86553c2210c24f5b3547a79
hitesh <hiteshkukaramprajapati@yahoo.com>
2025-07-24 16:54:03 +0530

    accounts app

---
+++ accounts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+     cantact = models.CharField(max_length=13)
+     gender = models.CharField(max_length=3,choices=(
+          ('M',"Male"),
+          ('F',"Female"),
+          ('o',"other"),
+          ('N/A',"Don't Want to mention")
+
+     ))
+     user_type = models.CharField(max_length=1,choices=(
+          ("S","Supplier"),
+          ("C","Customer")
+     ))
+
+
+class Address(models.Model):
+     title = models.CharField(max_length=40)
+    
+     address_line_one =models.CharField(max_length=50)
+     address_line_two =models.CharField(max_length=50)
+     city = models.CharField(max_length=50)
+     state = models.CharField(max_length=50)
+     pincode = models.IntegerField()
+     user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
--- qubits/settings.py
+++ qubits/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'main.apps.MainConfig'
+    'main.apps.MainConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL="accounts.User"
