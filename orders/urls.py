c01b9b668ac5685833720160418a1ad55e618ee7
hitesh <hiteshkukaramprajapati@yahoo.com>
2025-08-11 17:42:38 +0530

    cart view

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,6 @@ from .views import *
 from . import views 
 
 urlpatterns = [
-    path('add-to-cart/<int:product_id>/',views.add_to_cart,name="add_to_cart")
+    path('add-to-cart/<int:product_id>/',views.add_to_cart,name="add_to_cart"),
+    path('view-cart/',views.view_cart,name="view_cart"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,6 @@ from inventory.models import Product
 from .models import Cart
 from django.db.models import Q
 from django.contrib import messages
-# Create your views here.
 def add_to_cart(request,product_id):
     product = get_object_or_404(Product,id = product_id)
     cart = Cart.objects.filter( ( Q(user = request.user) & Q( product = product)))
@@ -15,3 +14,7 @@ def add_to_cart(request,product_id):
         cart[0].save()
         messages.success(request,f"Cart updated with Qauntity {cart[0].quantity } ")
     return redirect('product_details',product_id)
+
+def view_cart(request):
+    cart = Cart.objects.filter(user=request.user)
+    return render(request,'orders/cart.html',{'cart':cart})
